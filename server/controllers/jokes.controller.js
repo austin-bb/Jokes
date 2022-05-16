a1dd31d748dc173e4870e2850d85646b65df0568
Austinbb <austinbaumbach2@gmail.com>
2022-05-16 14:43:16 -0700

    commit

--- server/controllers/jokes.controller.js
+++ server/controllers/jokes.controller.js
@@ -2,30 +2,30 @@ const Joke = require("../models/jokes.model")
 
 module.exports.findAllJokes = (req, res) => {
     Joke.find()
-        .then(allJokes => res.json({ jokes: allJokes }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+        .then(allJokes => res.json(allJokes))
+        .catch(err => res.json(err));
 };
 
 module.exports.findOneJoke = (req, res) => {
     Joke.findOne({ _id: req.params.id })
-        .then(oneJoke => res.json({ joke: oneJoke }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+        .then(oneJoke => res.json(oneJoke))
+        .catch(err => res.json(err));
 };
 
 module.exports.createNewJoke = (req, res) => {
     Joke.create(req.body)
-        .then(newJoke => res.json({ joke: newJoke }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+        .then(newJoke => res.json(newJoke))
+        .catch(err => res.json(err));
 };
 
 module.exports.updateJoke = (req, res) => {
     Joke.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-        .then(updatedJoke => res.json({ joke: updatedJoke }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+        .then(updatedJoke => res.json(updatedJoke))
+        .catch(err => res.json(err));
 };
 
 module.exports.deleteJoke = (req, res) => {
     Joke.deleteOne({ _id: req.params.id })
-        .then(result => res.json({ result: result }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
 };
--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const JokeSchema = new mongoose.Schema({
 	setup: String,
 	punchline: String
-});
+}, { timestamps: true });
 
 const Joke = mongoose.model("joke", JokeSchema);
 
